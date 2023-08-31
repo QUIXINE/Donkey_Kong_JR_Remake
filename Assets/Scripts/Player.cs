ae4fe9db5d5f2678d9fc5f861c86400511148f24
QUIXINE <tiew470@gmail.com>
2023-08-31 22:13:18 +0700

    Player Movement
    1. Walking
    - bug: player still get input horizontal when not on ground
    2. Climbing
    - while climbing, gravity is 0, but will get 1 after getting back to the ground

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class  Player : MonoBehaviour
+sealed public class  Player : MonoBehaviour
 {
     //Other class access
     private Rigidbody2D rb;
@@ -9,6 +10,7 @@ public class  Player : MonoBehaviour
 
     //Input
     private float horizontal;
+    private float horizontalOnVine;
     private float vertical;
     
     private bool facingRight;       //Flipping
@@ -22,6 +24,7 @@ public class  Player : MonoBehaviour
     [Header("Climb")]
     [SerializeField] private float climbUpSpeed;
     [SerializeField] private float climbDownSpeed;
+    private bool isOnVine;
 
     [Header("Jump and Ground Check")]
     [SerializeField] private Transform groundCheckPos;
@@ -31,6 +34,7 @@ public class  Player : MonoBehaviour
 
     [Header("Vine Check")]
     [SerializeField] private Transform vineCheckPos;
+    [SerializeField] private Transform vineCheckPosOnHead;
     [SerializeField] private float vineCheckRadius;
     [SerializeField] private LayerMask vineLayerMask;
 
@@ -38,7 +42,6 @@ public class  Player : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     [SerializeField] Sprite onVineSprite;*/
     
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -53,30 +56,39 @@ public class  Player : MonoBehaviour
     void Update()
     {
         OnGround();
-        MoveInput();
+        GetInput();
         Jump();
         FlipInput();
+        HandleAnimation();
     }
 
     private void FixedUpdate()
     {
         Move();
+        Climb();
     }
 
     #region Inputs
-    private void MoveInput()
+    private void GetInput()
     {
         /* if (IsGrounded())*/ //Problem: only get input while on the ground, this makes when not on ground player move on itself because it takes the last input
-        if (!OnVine())
+        if (!OnVine() && IsGrounded())
             horizontal = Input.GetAxisRaw("Horizontal"); //--> change to DKHorizontal
         if (OnVine())
+        {
             vertical = Input.GetAxisRaw("Vertical");
-
-        if (horizontal > 0 || horizontal < 0) //if move, player will get out of the first state
-            animator.SetTrigger("MoveOut");
-
-        animator.SetFloat("Move", horizontal); 
-        animator.SetFloat("Climb", vertical); 
+           /* horizontalOnVine = Input.GetAxisRaw("Horizontal");*/ //--> change to DKHorizontal
+        }
+        
+        
+        if (OnVine() && Input.GetKeyDown(KeyCode.LeftArrow) && !facingRight)
+        {
+            Flip();
+        }
+        else if (OnVine() && Input.GetKeyDown(KeyCode.RightArrow) && facingRight)
+        {
+            Flip();
+        }
     }
 
     private void FlipInput()
@@ -89,51 +101,121 @@ public class  Player : MonoBehaviour
         {
             Flip();
         }
-    }
-
-    private bool OnVine()
-    {
-        return Physics2D.OverlapCircle(vineCheckPos.position, vineCheckRadius, vineLayerMask);
+        /*else if (OnVine() && horizontalOnVine < 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (OnVine() && horizontalOnVine > 0 && facingRight)
+        {
+            Flip();
+        }*/ //used with horizontalOnVine
     }
     #endregion
+
+    #region Checkers
+    private bool OnVine()
+    {
+        //check if player jump from the sides of vine
+        return Physics2D.Raycast(vineCheckPos.position, -vineCheckPos.right, vineCheckRadius, vineLayerMask);
+    }
+    private bool CollideVineOnHead()
+    {
+        //check if player jump from under of vine
+        return Physics2D.Raycast(vineCheckPosOnHead.position, vineCheckPos.up, vineCheckRadius, vineLayerMask);
+    }
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, groundLayerMask);
+        //return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, groundLayerMask);
+        return Physics2D.OverlapBox(groundCheckPos.position, new Vector2(0.8f,0f), 0, groundLayerMask);
     }
-    #region Movement
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
+        //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
+        Gizmos.DrawWireCube(groundCheckPos.position, transform.localScale);
+    }
     private void OnGround()
     {
         if (IsGrounded())
         {
+            rb.gravityScale = 1;
             animator.SetBool("OnVine", false);
             moveSpeed = firstMoveSpeed;
             animator.SetBool("Jump", false);
             animator.SetFloat("Move", 0);
+            vertical = 0;
+            isOnVine = false;
         }
     }
+    #endregion
+
+    #region Movement
     private void Move()
+    {
+        //On ground move
+        rb.velocity = new Vector2(horizontal * moveSpeed * Time.deltaTime, rb.velocity.y);
+        /* if(horizontal == 1 || horizontal == -1)
+         transform.Translate(horizontal * moveSpeed * Time.deltaTime, 0, 0);*/  //Try not use rb
+    }
+
+    private void Climb()
     {
         //Climbing
         if (OnVine())
         {
+            rb.gravityScale = 0;
             horizontal = 0;
             animator.SetBool("OnVine", true);
-            rb.velocity = new Vector2(rb.velocity.x, (vertical == 1? vertical * climbUpSpeed : vertical * climbDownSpeed) * Time.deltaTime);
+
+            //Condition to make Dk jr's hands don't go off the vine while OnVine, but is in the boundary of the vine
+            if (isOnVine == false)
+            {
+                if (transform.rotation == Quaternion.Euler(0, -180, 0))
+                {
+                    Vector2 pos = transform.position;
+                    pos.x -= 0.02f;
+                    transform.position = pos;
+                }
+                else if (transform.rotation == Quaternion.Euler(0, 0, 0))
+                {
+                    Vector2 pos = transform.position;
+                    pos.x += 0.02f;
+                    transform.position = pos;
+                }
+                isOnVine = true;
+            }
+            rb.velocity = new Vector2(rb.velocity.x, (vertical == 1 ? vertical * climbUpSpeed : vertical * climbDownSpeed) * Time.deltaTime);
         }
+        else if (CollideVineOnHead())
+        {
+            rb.gravityScale = 0;
+            horizontal = 0;
+            animator.SetBool("OnVine", true);
 
-        //On grpund move
-        rb.velocity = new Vector2(horizontal * moveSpeed * Time.deltaTime, rb.velocity.y);
-
-
-        /* if(horizontal == 1 || horizontal == -1)
-         transform.Translate(horizontal * moveSpeed * Time.deltaTime, 0, 0);*/  //Try not use rb
+            //Condition to make Dk jr's hands don't go off the vine while OnVine, but is in the boundary of the vine
+            if (isOnVine == false)
+            {
+                if (transform.rotation == Quaternion.Euler(0, -180, 0))
+                {
+                    Vector2 pos = transform.position;
+                    pos.x -= 0.2f;
+                    pos.y += 0.4f;
+                    transform.position = pos;
+                }
+                else if (transform.rotation == Quaternion.Euler(0, 0, 0))
+                {
+                    Vector2 pos = transform.position;
+                    pos.x += 0.2f;
+                    pos.y += 0.4f;
+                    transform.position = pos;
+                }
+                isOnVine = true;
+            }
+            rb.velocity = new Vector2(rb.velocity.x, (vertical == 1 ? vertical * climbUpSpeed : vertical * climbDownSpeed) * Time.deltaTime);
+        }
     }
-    void Flip()
-    {
-        facingRight = !facingRight;
-        transform.Rotate(0f, 180f, 0f);
-        
-    }
+    
     private void Jump()
     {
         if (Input.GetKeyDown(KeyCode.X) && IsGrounded())
@@ -146,32 +228,44 @@ public class  Player : MonoBehaviour
     }
     #endregion
 
-  
-    private void OnTriggerEnter2D(Collider2D col)
+    
+    void Flip()
     {
-        if (col.gameObject.name == "Vine")
-        {
-            //rb.gravityScale = 0;
-            /*rb.gravityScale = 0;
-            rb.isKinematic = true;*/
-            animator.SetBool("OnVine", true);
-            rb.gravityScale = 0;
-            //rb.simulated = false;
-            
-        }
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+
     }
-    private void OnTriggerExit2D(Collider2D col)
+
+    private void HandleAnimation()
     {
-        if (col.gameObject.name == "Vine")
-        {
-            //rb.gravityScale = 0;
-            rb.gravityScale = 1;
-            //rb.isKinematic = true;
-            //rb.simulated = true;
-            
-        }
+        if (horizontal > 0 || horizontal < 0) //if move, player will get out of the first state
+            animator.SetTrigger("MoveOut");
+
+        animator.SetFloat("Move", horizontal);
+        animator.SetFloat("Climb", vertical);
     }
 }
 
 //Problem: gravity after jump 
 //Problem: off the ground, player don't stop moving
+//Problem: fliping doesn't move sprite to left or right side of the vine, but only flip itself
+//Problem: In original game, after get down from the vine to the ground, can't move for a short time, but now mine can move
+//immediately after get to the ground
+//Problem : if OnGround and hold the x (jump), the jump animation will continue to play, and by getting on vine then get back to the ground
+
+//8/31/2023
+//make dk jr's hands are on the vine within the its boundary
+
+/*Check layermask
+private LayerMask GetLayerMask()
+{
+    RaycastHit2D hitInfo = Physics2D.Raycast(groundLayerCheckPos.position, groundLayerCheckPos.up * -1, groundCheckRadius);
+    LayerMask mask = new LayerMask();
+    if (hitInfo.collider != null)
+    {
+        mask.value = hitInfo.collider.gameObject.layer;
+        Debug.Log(hitInfo.collider.gameObject.layer);
+        return hitInfo.collider.gameObject.layer;
+    }
+    return mask;
+}*/
